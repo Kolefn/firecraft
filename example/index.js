08cfc563612d9636e41dde9b99e284c4bbfe21e0
Kole Nunley <Kfnunley@gmail.com>
2018-10-17 21:39:22 -0400

    Documentation

--- example/index.js
+++ example/index.js
@@ -1,7 +1,6 @@
 const functions = require('../lib/functions');
 
 
-
+functions.firestore.collection.node('add', (col, num1, num2) =>  num1 + num2 );
+let collection = new functions.firestore.collection("users/{userId}");
 let docs = functions.firestore.createDocuments({name: 'path/to/document'});
-
-console.log(docs);
