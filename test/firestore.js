cb9f2832fed267310add13d9b62d393996d7fa52
Kole Nunley <Kfnunley@gmail.com>
2018-10-21 17:59:03 -0400

    document development

--- test/firestore.js
+++ test/firestore.js
@@ -6,11 +6,11 @@ chai.should();
 describe('firestore', function(){
   describe('#createDocuments()', function(){
     it('should produce an object for each path supplied.', function(){
-      return firestore.createDocuments({doc1: 'path1', doc2: 'path2'}).should.have.all.keys('doc1', 'doc2');
+      return firestore.createDocuments({doc1: 'first/{path}', doc2: 'second/{path}'}).should.have.all.keys('doc1', 'doc2');
     });
 
     it('should produce valid document objects', function(){
-      return firestore.createDocuments({doc1: 'path1'}).doc1.should.be.an.instanceOf(firestore.collection.document);
+      return firestore.createDocuments({doc1: 'col/{docId}'}).doc1.should.be.an.instanceOf(firestore.collection.document);
     });
   });
 })
