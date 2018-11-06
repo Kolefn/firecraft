654d945ddf9ceb269a76a9ca7d54f7bace911cef
Kole Nunley <Kfnunley@gmail.com>
2018-11-06 15:44:30 -0500

    firebase project struct. to allow travis-ci to run

--- example/index.js
+++
@@ -1,6 +0,0 @@
-const functions = require('../lib/functions');
-
-
-functions.firestore.collection.node('add', (col, num1, num2) =>  num1 + num2 );
-let collection = new functions.firestore.collection("users/{userId}");
-let docs = functions.firestore.createDocuments({name: 'path/to/document'});
